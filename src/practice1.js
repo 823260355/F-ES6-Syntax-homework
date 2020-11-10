4d7b7ae1d864a5bb8a1e88a8179b380b2e5ce6da
liyuan <823260355@qq.com>
2020-11-10 21:04:09 +0800

    [liyuan] finish practice2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
-export { parseData };
+const parseData = input => {
+    var output = []
+    const { data, column } = input
+    for (let value of data) {
+        let item = {}
+        for (let [index, field] of value.entries()) {
+        item[column[index].name] = field
+      }
+      output.push(item)
+    }
+    return output
+  }
+  
+  export { parseData }
